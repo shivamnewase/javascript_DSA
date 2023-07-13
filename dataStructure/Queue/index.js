4721315f79ac4f562ecdb083c71ec0a91040cd69
shivamnewase <newaseshivam999@gmail.com>
2023-07-13 11:27:50 +0530

    implementation of queue using object

--- dataStructure/Queue/index.js
+++ dataStructure/Queue/index.js
@@ -1,42 +1,97 @@
+//Queue implementation using Array
+
+// class Queue {
+//     constructor(){
+//          this.items = [];
+//     }
+//     enqueue(element){
+//         this.items.push(element);
+//     }
+//     dequeue(){
+//         return this.items.shift();
+//     }
+   
+//     isEmpty(){
+//         return this.items.length ===0
+//       }
+
+//     peek(){
+//         if(this.isEmpty()){
+//             this.items[0]
+//         }
+//         return null;
+//     }
+//     size(){
+//         return this.items.length;
+//      }
+
+//      print(){
+//         console.log(this.items.toString())
+//      }
+ 
+// }
+
+// const queue = new Queue();
+
+// console.log(queue.isEmpty());
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// console.log(queue.size());
+// console.log(queue.print());
+// queue.dequeue();
+
+// console.log(queue.print());
+
+
+
+//Queue Implementation using Object
+
+
 class Queue {
     constructor(){
-         this.items = [];
+         this.items = {};
+         this.rear = 0;
+         this.front = 0;
     }
-    enqueue(element){
-        this.items.push(element);
-    }
-    dequeue(){
-        return this.items.shift();
-    }
-   
-    isEmpty(){
-        return this.items.length ===0
-      }
 
+    enqueue(element) {
+        this.items[this.rear] = element
+        this.rear++
+    }
+
+    dequeue(){
+        const item = this.items[this.front];
+        delete this.items[this.front]
+        this.front++
+        return item
+    }
+
+    isEmpty(){
+         return this.rear - this.front === 0
+    }
     peek(){
-        if(this.isEmpty()){
-            this.items[0]
-        }
-        return null;
+         return this.items[this.front]
     }
     size(){
-        return this.items.length;
-     }
-
-     print(){
-        console.log(this.items.toString())
-     }
- 
+        return this.rear-this.front
+    }
+    print(){
+        console.log(this.items)
+    }
 }
 
-const queue = new Queue();
+const queue = new Queue()
 
-console.log(queue.isEmpty());
-queue.enqueue(10);
+console.log(queue.isEmpty())
+
 queue.enqueue(20);
 queue.enqueue(30);
-console.log(queue.size());
-console.log(queue.print());
-queue.dequeue();
+queue.enqueue(40);
 
-console.log(queue.print());
+console.log(queue.items)
+
+queue.dequeue();
+queue.enqueue(50);
+queue.dequeue();
+console.log(queue.items)
